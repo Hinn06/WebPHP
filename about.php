4e3877204361b21f62e8a52d4093cbf93560d8b9
Hinn06 <chiennhuxk09@gmail.com>
2026-08-10 16:14:00 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Giới thiệu bản thân</title>
+</head>
+<body>
+    <h1>Giới thiệu bản thân</h1>
+
+    <p>Xin chào, mình là Hiền.</p>
+    <p>Mình đang học lập trình web với PHP.</p>
+
+    <h2>Dự án đã thực hiện</h2>
+    <ul>
+        <li>Website quản lý cửa hàng nhỏ</li>
+        <li>Website đặt tour</li>
+        <li>Website quản lý thư viện</li>
+        <li>Website quản lý phòng khám nhỏ</li>
+        <li>Website quản lý khóa học và đăng ký học phần</li>
+    </ul>
+</body>
+</html>
